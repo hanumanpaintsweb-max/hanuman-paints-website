fc9a45ad5cfd1f6e73e207d784f4305b33d678a9
Sachin kumar <iamsachinkumar2025@gmail.com>
2026-06-06 17:09:39 +0530

    feat: Sentry error monitoring

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,7 +2,12 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1,
+  ignoreErrors: [
+    'ResizeObserver loop limit exceeded',
+  ],
+  denyUrls: [/\/admin\//],
+  tracesSampleRate: 0.1,
+  environment: process.env.NODE_ENV,
   debug: false,
   replaysOnErrorSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
